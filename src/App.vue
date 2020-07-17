6503bb4ee0bf17cc59b695e91729d33bf6112318
Parth Sharma <parthsharma151@gmail.com>
2020-07-17 16:58:32 +0530

    added background and styling

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,26 @@ export default {
 
 <style>
 
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-body{
-  font-family: Arial, Helvetica, sans-serif;
-}
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+  }
 
-#app {
-  background-image: url('./assets/cold-bg.jpg');
-}
+  #app {
+    background-image: url('./assets/cold-bg.jpg');
+    background-size: cover;
+    background-position: top;
+    transition: 0.4s;
+  }
+
+  main {
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+  }
 </style>
