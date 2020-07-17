d97d96ee9e445a7c4bd4e80413a1860afc6f5435
Parth Sharma <parthsharma151@gmail.com>
2020-07-17 20:15:20 +0530

    additional styling'
    end

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
 
   *{
     margin: 0;
-    padding: 0;
+    padding: 0px;
     box-sizing: border-box;
   }
 
@@ -43,5 +43,29 @@ export default {
     min-height: 100vh;
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+  }
+
+  .search-box {
+    width: 100%;
+    margin: 50px;
+  }
+
+  .search-box .search-bar {
+    display: block;
+    align-content: center;
+    width: 100%;
+    padding: 15px;
+
+    color: #313131;
+    font-size: 20px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+
+    background-color: rgba(255, 255, 255, 0.9);
+    border-radius: 12px 12px 12px 12px;
+
   }
 </style>
