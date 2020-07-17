23635397d08831046f0cc5dbe2c545941211e988
Parth Sharma <parthsharma151@gmail.com>
2020-07-18 00:39:17 +0530

    Added support for dates and temperature

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,12 @@
 
         <div class="location-box">
           <div class="location"> {{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Saturday — July 18, 2020</div>
+          <div class="date">{{dateBuilder()}}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9° C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}° C</div>
+          <div class="weather">{{weather.weather[0].main }}</div>
         </div>
 
       </div>
@@ -55,6 +55,16 @@ export default {
     },
     setResults (results) {
       this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} — ${date} ${month}, ${year}`;
     }
   }
 }
