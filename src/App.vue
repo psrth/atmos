68519ca56a25ddb1edf7d61383039114a85cf28b
Parth Sharma <parthsharma151@gmail.com>
2020-07-17 16:43:47 +0530

    API key and app build

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
-    
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..."/>
+        
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      api_key: '39baf1397c2e0a511fc699715cb417b6'
+    }
+  }
 }
 </script>
 
 <style>
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+}
 </style>
