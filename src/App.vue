eddbe40168c94648a7dd112a34cef80637cd6fbf
Parth Sharma <parthsharma151@gmail.com>
2020-07-18 12:10:15 +0530

    added comments

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+// ATMOS 1.0 — parth sharma, 18-7-20
+// this entire project follows the Vue.js project structure
+
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 20 ? 'warm' : ''">
     <main>
@@ -6,7 +9,7 @@
         <div class="container"><h1>Atmos 🌦</h1></div>
       </div> -->
 
-
+      <!-- div containing the search box and sending query -->
       <div class="search-box">
         <input 
           type="text" 
@@ -17,14 +20,17 @@
           />
       </div>
 
+      <!-- only displays weather box if entered location is defined -->
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
 
         <div class="location-box">
+          <!-- fetches location, country, and shows date -->
           <div class="location"> {{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">{{dateBuilder()}}</div>
         </div>
 
         <div class="weather-box">
+          <!-- shows the int value of temperature and the weather type -->
           <div class="temp">{{Math.round(weather.main.temp)}}° C</div>
           <div class="weather">{{weather.weather[0].main }}</div>
         </div>
@@ -43,29 +49,41 @@
 export default {
   name: 'App',
 
+  // data method
   data () {
 
     return {
+
+      // used from openweathermap.org 
       api_key: '39baf1397c2e0a511fc699715cb417b6',
       url_base: 'https://api.openweathermap.org/data/2.5/',
 
+      // returns a query
       query: '',
       weather: {}
     }
   },
 
   methods: {
+
+    // fetch weather method
     fetchWeather (e) {
+
+      // method only called if enter key is pressed
       if (e.key == "Enter") {
+
+        // query sent to API to return val in json file
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
       }
     },
+    // results are set
     setResults (results) {
       this.weather = results;
     },
+    // date builder function to diplay string format of date
     dateBuilder() {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -82,6 +100,8 @@ export default {
 
 <style>
 
+  /* CSS stylesheet for the markup */
+  
   *{
     margin: 0;
     padding: 0px;
